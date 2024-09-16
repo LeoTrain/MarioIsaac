ef15848033e84e8eb50c4468dc967fecc857f035
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-09-16 21:19:29 +0200

    reformated select_image, thx for the idea bro!

--- bll/animation_controller.py
+++ bll/animation_controller.py
@@ -25,46 +25,24 @@ class AnimationController:
         if self.current_frame_index > total_frames:
             self.current_frame_index = 0
 
-    def _apply_mask(self):
+    def _update_mask(self):
         self.mask = pygame.mask.from_surface(self.image)
 
-    def _select_idle_image(self):
-        frame_count = self.frame_counts.get("idle", 12)
+    def _select_state_image(self) -> None:
+        frame_count = self.frame_counts.get(self.current_state, 0)
+        state_up = self.current_state + "_up"
+        state_down_right = self.current_state + "_down_right"
+        state_down_left = self.current_state + "_down_left"
         if self.current_y_direction == "up":
-            self.image = self.sprites["idle_up"][self.current_frame_index // frame_count]
+            self.image = self.sprites[state_up][self.current_frame_index // frame_count]
         else:
             if self.current_x_direction == "right":
-                self.image = self.sprites["idle_down_right"][self.current_frame_index // frame_count]
-            elif self.current_x_direction == "left":
-                self.image = self.sprites["idle_down_left"][self.current_frame_index // frame_count]
-
-    def _select_run_image(self):
-        frame_count = self.frame_counts.get("run", 8)
-        if self.current_y_direction == "up":
-            self.image = self.sprites["run_up"][self.current_frame_index // frame_count]
-        else:
-            if self.current_x_direction == "right":
-                self.image = self.sprites["run_down_right"][self.current_frame_index // frame_count]
-            elif self.current_x_direction == "left":
-                self.image = self.sprites["run_down_left"][self.current_frame_index // frame_count]
-
-    def _select_attack_image(self):
-        frame_count = self.frame_counts.get("attack", 8)
-        if self.current_y_direction == "up":
-            self.image = self.sprites["attack_up"][self.current_frame_index // frame_count]
-        else:
-            if self.current_x_direction == "right":
-                self.image = self.sprites["attack_down_right"][self.current_frame_index // frame_count]
-            elif self.current_x_direction == "left":
-                self.image = self.sprites["attack_down_left"][self.current_frame_index // frame_count]
+                self.image = self.sprites[state_down_right][self.current_frame_index // frame_count]
+            else:
+                self.image = self.sprites[state_down_left][self.current_frame_index // frame_count]
 
     def _select_image(self):
-        if self.current_state == "idle":
-            self._select_idle_image()
-        elif self.current_state == "run":
-            self._select_run_image()
-        elif self.current_state == "attack":
-            self._select_attack_image()
+        self._select_state_image()
         self._update_rectangle()
 
     def _update_rectangle(self):
@@ -75,4 +53,4 @@ class AnimationController:
     def update_sprite(self):
         self._increment_frame()
         self._select_image()
-        self._apply_mask()
+        self._update_mask()
