e0178c4cd45487ece525a8ab427480d3cf31eb66
ftleo <leonardo.bertonasco01@gmail.com>
2024-09-30 10:45:59 +0200

    closer to the walls

--- logic/collision_handler.py
+++ logic/collision_handler.py
@@ -30,18 +30,19 @@ class CollisionHandler:
 
     def _handle_horizontal_collision(self) -> bool:
         collision_detected = False
+        player_center = self.player.rect.width * 0.2
         for tile in self.collision_tiles:
             if pygame.sprite.collide_mask(self.player, tile):
                 if self.player.last_pressed_direction == "right":
                     overlap = self.player.rect.right - tile.rect.left
                     if overlap > 0:
-                        self.player.rect.right = tile.rect.left
+                        self.player.rect.right = tile.rect.left + player_center
                         self.player.can_move_right = False
 
                 elif self.player.last_pressed_direction == "left":
                     overlap = self.player.rect.right - tile.rect.left
                     if overlap > 0:
-                        self.player.rect.left = tile.rect.right
+                        self.player.rect.left = tile.rect.right - player_center
                     self.player.can_move_left = False
                 collision_detected = True
 
@@ -55,18 +56,19 @@ class CollisionHandler:
 
     def _handle_vertical_collision(self) -> bool:
         collision_detected = False
+        player_center = self.player.rect.height * 0.2
         for tile in self.collision_tiles:
             if pygame.sprite.collide_mask(self.player, tile):
                 if self.player.last_pressed_direction == "down":
                     overlap = self.player.rect.bottom - tile.rect.top
                     if overlap > 0:
-                        self.player.rect.bottom = tile.rect.top
+                        self.player.rect.bottom = tile.rect.top + player_center
                         self.player.can_move_down = False
 
                 elif self.player.last_pressed_direction == "up":
                     overlap = tile.rect.bottom - self.player.rect.top
                     if overlap > 0:
-                        self.player.rect.top = tile.rect.bottom
+                        self.player.rect.top = tile.rect.bottom - player_center
                         self.player.can_move_up = False
                 collision_detected = True
 
