a1a8154347de8408fe09f6a329b5aa601d8ed6cb
ftleo <leonardo.bertonasco01@gmail.com>
2024-10-01 19:35:53 +0200

    trying to improve player to wall collision

--- logic/collision_manager.py
+++ logic/collision_manager.py
@@ -1,6 +1,7 @@
 from .collision_detector import CollisionDetector
 from .collision_effect_handler import CollisionEffectHandler
 from .collision_resolver import CollisionResolver
+from ..entities.player import Player
 
 
 class CollisionManager:
@@ -14,9 +15,16 @@ class CollisionManager:
             collided_tiles = self.collision_detector.detect_tile_collisions(entity, tiles)
             if collided_tiles:
                 self.collision_resolver.resolve_tile_collision(entity, collided_tiles)
-
+            else:
+                entity.can_move_up = True
+                entity.can_move_down = True
+                entity.can_move_left = True
+                entity.can_move_right = True
+                    
             for other_entity in entities:
                 if entity != other_entity:
                     if self.collision_detector.detect_entity_collision(entity, other_entity):
                         self.collision_resolver.resolve_entity_collision(entity, other_entity)
                         self.collision_effect_handler.handle_entity_collision_effects(entity, other_entity)
+
+
--- logic/collision_resolver.py
+++ logic/collision_resolver.py
@@ -1,41 +1,51 @@
+import pygame
+
+
 class CollisionResolver:
     def resolve_tile_collision(self, entity, tiles):
-        collision_detected = False
+        collision_left = False
+        collision_right = False
+        collision_up = False
+        collision_down = False
         for tile in tiles:
             overlap_x = entity.rect.centerx - tile.rect.centerx
             overlap_y = entity.rect.centery - tile.rect.centery
             if abs(overlap_x) > abs(overlap_y):
                 if overlap_x > 0:
                     entity.rect.left = tile.rect.right
-                    entity.can_move_left = False
-                    collision_detected = True
+                    collision_left = True
                 else:
                     entity.rect.right = tile.rect.left
-                    entity.can_move_right = False
-                    collision_detected = True
+                    collision_right = True
             else:
                 if overlap_y > 0:
                     entity.rect.top = tile.rect.bottom
-                    entity.can_move_up = False
-                    collision_detected = True
+                    collision_up = True
                 else:
                     entity.rect.bottom = tile.rect.top
-                    entity.can_move_down = False
-                    collision_detected = True
-
-        if not collision_detected:
-            entity.can_move_right = True
-            entity.can_move_left = True
-            entity.can_move_down = True
-            entity.can_move_up = True
+                    collision_down = True
+        if collision_left:
+            entity.can_move_left = False
+        if collision_right:
+            entity.can_move_right = False
+        if collision_up:
+            entity.can_move_up = False
+        if collision_down:
+            entity.can_move_down = False
 
     def resolve_entity_collision(self, entity1, entity2):
-        if entity1.rect.centerx < entity2.rect.centerx:
-            entity1.rect.right = entity2.rect.left
-        else:
-            entity1.rect.left = entity2.rect.right
+        overlap_x = entity1.rect.centerx - entity2.rect.centerx
+        overlap_y = entity1.rect.centery - entity2.rect.centery
 
-        if entity1.rect.centery < entity2.rect.centery:
-            entity1.rect.bottom = entity2.rect.top
+        if abs(overlap_x) > abs(overlap_y):
+            if overlap_x > 0:
+                entity1.rect.left = entity2.rect.right
+            else:
+                entity1.rect.right = entity2.rect.left
         else:
-            entity1.rect.top = entity2.rect.bottom
+            if overlap_y > 0:
+                entity1.rect.top = entity2.rect.bottom
+            else:
+                entity1.rect.bottom = entity2.rect.top
+
+
